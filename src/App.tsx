5d85e6224e88fa01e4850696966860e943992498
Magnusbruus <magnusbruus@gmail.com>
2024-01-29 14:22:56 +0100

    Albumpicker

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,41 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { FormEvent } from 'react'
+
+function AlbumPicker() {
+  const [albums, setAlbums] = useState<string[]>([]);
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      artist: { value: string };
+    };
+    const artist = encodeURIComponent(target.artist.value);
+    const url = `https://musicbrainz.org/ws/2/release?fmt=json&query=artist:${artist}`;
+    const response = await fetch(url);
+    const mbResult = (await response.json()) as {
+      releases: { title: string }[];
+    };
+    const { releases } = mbResult;
+    setAlbums(releases.map(({ title }) => title));
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Artist name:
+        <input name="artist" />
+      </label>
+      <button type="submit">Search</button>
+      <p>Albums:</p>
+      <AlbumPicker />
+      <ol>
+        {albums.map((album) => (
+          <li>{album}</li>
+        ))}
+      </ol>
+    </form>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
